f70be0dcec44cc4b63d7b8823c13cae7631abc38
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-07-07 09:15:01 -0500

    Controller and views check

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import usuarioRoutes from "./routes/usuarioRoutes.js";
+
+const app = express();
+//Habilitar Routing
+app.set("view engine", "pug");
+app.set("views", "./views");    //Especificamos el nombre de la carpeta y la ruta
+//Routing
+app.use("/auth", usuarioRoutes);
+const port = 4000;
+app.listen(port, () => {
+    console.log("El servidor está funcionando en el puerto 4000");
+});
