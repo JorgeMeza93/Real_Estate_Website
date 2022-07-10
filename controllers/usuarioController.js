b334b9a7cd691be4de5d53e2af1a6c0b92127c15
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-07-10 15:25:53 -0500

    Creacion de la conexión y modelo, variables de entorno y validaciones de campos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,12 +1,32 @@
-
+import Usuario from "../models/Usuario.js";
+import { check, validationResult } from "express-validator";
 
 const formularioLogin = (req, res) => {
-    res.render("auth/login");
+    res.render("auth/login", {
+        pagina: "Iniciar Sesión"
+    });
 }
 const formularioRegistro = (req, res) => {
     res.render("auth/registro", {
         pagina: "Crear Cuenta"
     })
 }
+const registrar = async (req, res) => {
+    //Validación
+    await check("nombre").notEmpty().withMessage("El nombre no puede ir vacío").run(req);
+    await check("email").isEmail().withMessage("Eso no parece un email").run(req);
+    await check("password").isLength({ min:6 }).withMessage("El password debe de ser al menos de 6 caracteres").run(req);
+    await check("repetir-password").equals("password").withMessage("Los passwords no son iguales").run(req)
+    let resultado = validationResult(req);
+    res.json( resultado.array());
+    const usuario = await Usuario.create(req.body);
+    console.log(usuario);
+    res.json(usuario)
+}
+const formularioOlvidePassword = (req, res) => {
+    res.render("auth/olvidePassword", {
+        pagina: "¿Olvidaste tu password? Recuperala"
+    })
+}
 
-export { formularioLogin, formularioRegistro };
+export { formularioLogin, formularioRegistro, formularioOlvidePassword, registrar };
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from "express";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+import db from "./config/db.js";
 
 const app = express();
-//Habilitar Routing
+// Habilitar lectura de datos de formularios
+app.use( express.urlencoded({ extended: true }))
+//Conexión a la base de datos
+try {
+    await db.authenticate();
+    db.sync();
+    console.log("Conexión exitosa a la base de datos")
+} 
+catch (error) {
+    console.log("Error en la conexión a la base de datos \n" + error);
+}
+//Carpeta pública
 app.use(express.static("public"));
+//Habilitar Pug
 app.set("view engine", "pug");
 app.set("views", "./views");    //Especificamos el nombre de la carpeta y la ruta
 //Routing
