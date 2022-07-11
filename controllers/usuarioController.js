0e3c2835fd7d70f67157f0c8d8b8a5a1d8eff9ba
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-07-11 01:14:42 -0500

    Hasheo de passwords, generacion de token, validacion de campos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,6 @@
 import Usuario from "../models/Usuario.js";
 import { check, validationResult } from "express-validator";
+import { generarID } from "../helpers/tokens.js";
 
 const formularioLogin = (req, res) => {
     res.render("auth/login", {
@@ -12,15 +13,44 @@ const formularioRegistro = (req, res) => {
     })
 }
 const registrar = async (req, res) => {
+    const password = req.body.password;
     //Validación
     await check("nombre").notEmpty().withMessage("El nombre no puede ir vacío").run(req);
     await check("email").isEmail().withMessage("Eso no parece un email").run(req);
     await check("password").isLength({ min:6 }).withMessage("El password debe de ser al menos de 6 caracteres").run(req);
-    await check("repetir-password").equals("password").withMessage("Los passwords no son iguales").run(req)
+    await check("repetir-password").equals(password).withMessage("Los passwords no son iguales").run(req)
     let resultado = validationResult(req);
-    res.json( resultado.array());
-    const usuario = await Usuario.create(req.body);
-    console.log(usuario);
+    //Verificacion del resultado este vacío
+    if(!resultado.isEmpty()){
+        //Si hay errotes
+        return res.render("auth/registro", {
+            pagina: "Crear Cuenta",
+            errores: resultado.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
+        })
+    }
+    //Si existe un correo ya registrado mandar mensaje de error
+    const existeUsuario = await Usuario.findOne({ where: { email: req.body.email } });
+    if(existeUsuario){
+        return res.render("auth/registro", {
+            pagina: "Crear Cuenta",
+            errores: [{msg: "El usuario ya esta registrado"}],
+            usuario: {
+                nombre: req.body.nombre,   // <-- Evitamos que los datos de los campos del formulario se pierdan
+                email: req.body.email
+            }
+        })
+    }
+    const usuario = await Usuario.create({
+        nombre: req.body.nombre,
+        email: req.body.email,
+        password: req.body.password,
+        token: generarID()
+
+    });
     res.json(usuario)
 }
 const formularioOlvidePassword = (req, res) => {
--- models/Usuario.js
+++ models/Usuario.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import db from "../config/db.js";
+import bcrypt from "bcrypt";
 
 // Tabla que se creará
 const Usuario = db.define("usuarios", {
@@ -17,6 +18,15 @@ const Usuario = db.define("usuarios", {
     },
     token: DataTypes.STRING,
     confirmado: DataTypes.BOOLEAN
-});
+},
+{
+    hooks: {
+        beforeCreate: async function(usuario){
+            const salt = await bcrypt.genSalt(10);
+            usuario.password = await bcrypt.hash( usuario.password, salt );
+        }
+    }
+}
+);
 
 export default Usuario;
