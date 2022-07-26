794a27e96d9c25ee2066efdc85079091b3f89234
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-07-26 18:07:40 -0500

    Creacion de los modelos de Propiedad, Categoria y Precio. Descripción en crear.pug

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -3,10 +3,32 @@
     const lat = 19.4030867;
     const lng = -99.1743499;
     const mapa = L.map('mapa').setView([lat, lng ], 15);
-
+    let marker;
+    //Utilizar provider y geocoder
+    const geocodeService = L.esri.Geocoding.geocodeService();
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+    //Colocar el pin 
+    marker = new L.marker([lat, lng], {
+        draggable: true,
+        autoPan: true
+    })
+    .addTo(mapa)
+    //Detectar el movimiento del pin
+    marker.on("moveend", function (e){
+        marker = e.target;
+        const posicion = marker.getLatLng();
+        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng));
+        //Obtener información de las calles al soltar el pin
+        geocodeService.reverse().latlng(posicion, 15).run(function(error, resultado){
+            console.log(resultado);
+            marker.bindPopup(resultado.address.LongLabel);
+            //Llenar los campos
+            document.querySelector(".calle").textContent = resultado?.address?.Address ?? "";
+            document.querySelector("#calle").value = resultado?.latlng?.lat ?? "";
+            document.querySelector("#lng").value = resultado?.latlng?.lng ?? "";
 
-
+        });
+    })
 })()
